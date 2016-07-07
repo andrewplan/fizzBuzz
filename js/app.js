e203b00befcdf1255f6c56f91a5645341b3c6d67
andrewplan <a.plan88@gmail.com>
2016-07-07 14:32:09 -0500

    Modified FizzBuzz app to meet challenge requirements.

--- js/app.js
+++ js/app.js
@@ -1,20 +1,37 @@
 $(document).ready(function () {
-    for (var i = 1; i <= 100; i++ ) {
-        if (i % 3 === 0 && i % 5 === 0 && i !== 0) {
-            console.log("fizzbuzz");
-            $( "body" ).append( "<p>fizzbuzz</p>" );
-        }
-        else if (i % 3 === 0) {
-            console.log("fizz");
-            $( "body" ).append( "<p>fizz</p>" );
-        }
-        else if (i % 5 === 0) {
-            console.log("buzz");
-            $( "body" ).append( "<p>buzz</p>" );
+    'use strict'
+    
+    var integerInput = +(prompt("Please enter an integer in between 1 and 100."));
+    console.log(integerInput);
+    
+    function fizzBuzz (ints) {
+        if (ints === NaN || (ints % 1 !== 0)) {
+            alert("You must enter in an integer!");
+            ints = +(prompt("Please enter an integer in between 1 and 100."));
+            fizzBuzz(ints);
         }
         else {
-            console.log(i);
-            $( "body" ).append( "<p>" + i + "</p>" );
+            for (var i = 1; i <= ints; i++ ) {
+                if (i % 3 === 0 && i % 5 === 0 && i !== 0) {
+                    console.log("fizzbuzz");
+                    $( "body" ).append( "<p>fizzbuzz</p>" );
+                }
+                else if (i % 3 === 0) {
+                    console.log("fizz");
+                    $( "body" ).append( "<p>fizz</p>" );
+                }
+                else if (i % 5 === 0) {
+                    console.log("buzz");
+                    $( "body" ).append( "<p>buzz</p>" );
+                }
+                else {
+                    console.log(i);
+                    $( "body" ).append( "<p>" + i + "</p>" );
+                }
+            }
         }
     }
+    
+    fizzBuzz(integerInput);
+    
 });
